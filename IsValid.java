8d0079e4090ccc5d199f87e840869f19473e7871
ChrisLyn <saffercsmith@gmail.com>
2018-02-24 00:38:31 -0700

    Valid Parenthesis #20

---
+++ IsValid.java
@@ -0,0 +1,27 @@
+class IsValid {
+    public boolean isValid(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '[') {
+                stack.push(']');
+            } else if (s.charAt(i) == '{') {
+                stack.push('}');
+            } else if (s.charAt(i) == '(') {
+                stack.push(')');
+            } else {
+                if (stack.empty()) {
+                    return false;
+                }
+                if (s.charAt(i) != stack.peek()) {
+                    return false;
+                }
+                stack.pop();
+            }
+            
+        }
+        return stack.size() == 0;
+    }
+}
